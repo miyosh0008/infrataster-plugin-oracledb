87e49c4eeedb8ceb7e6247be0692ac26652b9f8f
yoshidamct <yoshidamct@intellilink.co.jp>
2016-08-27 17:10:12 +0900

    Added infrataster-plugin-oracledb.rb

---
+++ lib/infrataster-plugin-oracledb.rb
@@ -0,0 +1,5 @@
+# infrataster-plugin-oracledb.rb
+# coding: utf-8
+require 'infrataster/resources/oracledb_query_resource'
+require 'infrataster/contexts/oracledb_query_context'
+require 'infrataster/helpers/oracledb_resource_helper'
